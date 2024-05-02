aeaef175152c02c24b484b00f81a1abf4f874874
Sai <126769938+saikam2003@users.noreply.github.com>
2024-05-02 19:47:36 +1200

    trying to add clouds

--- Project/background.vhd
+++ Project/background.vhd
@@ -7,18 +7,30 @@ USE  IEEE.STD_LOGIC_SIGNED.all;
 ENTITY background IS
 	PORT
 		( clk, vert_sync, horz_sync	: IN std_logic;
-          pixel_row, pixel_column	: IN std_logic_vector(9 DOWNTO 0);
+		  pixel_row, pixel_column	: IN std_logic_vector(9 DOWNTO 0);
 		  red, green, blue 			: OUT std_logic);		
 END background;
 
 architecture behavior of background is
 
+SIGNAL cloud_on					: std_logic;
+SIGNAL size 					: std_logic_vector(9 DOWNTO 0);  
+SIGNAL cloud_y_pos				: std_logic_vector(9 DOWNTO 0);
+SiGNAL cloud_x_pos				: std_logic_vector(10 DOWNTO 0);
+
 BEGIN           
 
+size <= CONV_STD_LOGIC_VECTOR(20,10);
+
+cloud_x_pos <= CONV_STD_LOGIC_VECTOR(540,11);
+
+cloud_on <= '1' when ( ('0' & cloud_x_pos <= '0' & pixel_column + size) and ('0' & pixel_column <= '0' & cloud_x_pos + size) 	-- x_pos - size <= pixel_column <= x_pos + size
+					and ('0' & cloud_y_pos <= pixel_row + size) and ('0' & pixel_row <= cloud_y_pos + size) )  else	-- y_pos - size <= pixel_row <= y_pos + size
+			'0';
 -- Colours for pixel data on video signal
--- Changing the background and ball colour by pushbuttons
-Red <=  '0';
+
+Red <=  cloud_on;
 Green <= '1';
 Blue <=  '1';
-END behavior;
 
+END behavior;
