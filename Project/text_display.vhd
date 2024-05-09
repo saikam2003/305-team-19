ab0eaec97bbc658d4be1468b609d29ec0733cdfb
vdis663 <126431339+vdis663@users.noreply.github.com>
2024-05-09 15:56:33 +1200

    got two text of different sizes working

--- Project/text_display.vhd
+++ Project/text_display.vhd
@@ -11,6 +11,8 @@ END ENTITY;
 
 ARCHITECTURE behaviour OF text_display IS
 	SIGNAL t_text_on: STD_LOGIC;
+	SIGNAL size_row, size_col: STD_LOGIC_VECTOR(2 downto 0);
+	SIGNAL char_address_1, char_address_2, char_address_final: STD_LOGIC_VECTOR (5 DOWNTO 0);
 	
 	COMPONENT char_rom IS
 		PORT(
@@ -23,9 +25,9 @@ ARCHITECTURE behaviour OF text_display IS
 BEGIN
 	
 	char_comp: char_rom PORT MAP(
-		character_address => pixel_row(9 downto 4),
-		font_row => pixel_row(3 downto 1),
-		font_col => pixel_column(3 downto 1),
+		character_address => char_address_final,
+		font_row => size_row,
+		font_col => size_col,
 		clock => clk,
 		rom_mux_output => t_text_on);
 		
@@ -33,7 +35,31 @@ BEGIN
 	green <= '0';
 	
 	red <= t_text_on;
-	text_on <= t_text_on;
+	text_on <= t_text_on when (pixel_row <= 47 and pixel_column <= 300) else '0';
+	char_address_final <= char_address_2 when (pixel_row <= 47 and pixel_row > 31) else char_address_1;
+	size_row <= pixel_row(3 downto 1) when (pixel_row <= 47 and pixel_row > 31) else pixel_row(4 downto 2) ;
+	size_col <= pixel_column(3 downto 1) when (pixel_row <= 47 and pixel_row > 31) else pixel_column(4 downto 2) ;
+	with pixel_column(9 downto 5) select
+		char_address_1 <= CONV_STD_LOGIC_VECTOR(7,6) when CONV_STD_LOGIC_VECTOR(0,5),
+							 CONV_STD_LOGIC_VECTOR(1,6) when CONV_STD_LOGIC_VECTOR(1,5),
+							 CONV_STD_LOGIC_VECTOR(13,6) when CONV_STD_LOGIC_VECTOR(2,5),
+							 CONV_STD_LOGIC_VECTOR(5,6) when CONV_STD_LOGIC_VECTOR(3,5),
+							 CONV_STD_LOGIC_VECTOR(32,6) when CONV_STD_LOGIC_VECTOR(4,5),
+							 CONV_STD_LOGIC_VECTOR(15,6) when CONV_STD_LOGIC_VECTOR(5,5),
+							 CONV_STD_LOGIC_VECTOR(22,6) when CONV_STD_LOGIC_VECTOR(6,5),
+							 CONV_STD_LOGIC_VECTOR(5,6) when CONV_STD_LOGIC_VECTOR(7,5),
+							 CONV_STD_LOGIC_VECTOR(18,6) when CONV_STD_LOGIC_VECTOR(8,5),
+							 CONV_STD_LOGIC_VECTOR(32,6) when others;
+							 
+	with pixel_column(8 downto 4) select
+		char_address_2 <= CONV_STD_LOGIC_VECTOR(18,6) when CONV_STD_LOGIC_VECTOR(0,5),
+							 CONV_STD_LOGIC_VECTOR(5,6) when CONV_STD_LOGIC_VECTOR(1,5),
+							 CONV_STD_LOGIC_VECTOR(19,6) when CONV_STD_LOGIC_VECTOR(2,5),
+							 CONV_STD_LOGIC_VECTOR(20,6) when CONV_STD_LOGIC_VECTOR(3,5),
+							 CONV_STD_LOGIC_VECTOR(1,6) when CONV_STD_LOGIC_VECTOR(4,5),
+							 CONV_STD_LOGIC_VECTOR(18,6) when CONV_STD_LOGIC_VECTOR(5,5),
+							 CONV_STD_LOGIC_VECTOR(20,6) when CONV_STD_LOGIC_VECTOR(6,5),
+							 CONV_STD_LOGIC_VECTOR(32,6) when others;
 END ARCHITECTURE behaviour;
 			
 		
