65ac7de65e9aa4ad1e8bc2e1d3a9369ab103ced5
Sai <126769938+saikam2003@users.noreply.github.com>
2024-05-14 17:26:48 +1200

    Trying to figure out sprite logic

--- Project/bird_rom.vhd
+++ Project/bird_rom.vhd
@@ -9,17 +9,16 @@ USE altera_mf.all;
 ENTITY bird_rom IS
 	PORT
 	(
-		character_address	:	IN STD_LOGIC_VECTOR (5 DOWNTO 0);
-		font_row, font_col	:	IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+		font_row, font_col	:	IN STD_LOGIC_VECTOR (3 DOWNTO 0);
 		clock				: 	IN STD_LOGIC ;
-		rom_mux_output		:	OUT STD_LOGIC
+		bird_data_output		:	OUT STD_LOGIC
 	);
 END bird_rom;
 
 
 ARCHITECTURE SYN OF char_rom IS
 
-	SIGNAL rom_data		: STD_LOGIC_VECTOR (7 DOWNTO 0);
+	SIGNAL rom_data		: STD_LOGIC_VECTOR (15 DOWNTO 0);
 	SIGNAL rom_address	: STD_LOGIC_VECTOR (8 DOWNTO 0);
 
 	COMPONENT altsyncram
@@ -41,8 +40,8 @@ ARCHITECTURE SYN OF char_rom IS
 	);
 	PORT (
 		clock0		: IN STD_LOGIC ;
-		address_a	: IN STD_LOGIC_VECTOR (8 DOWNTO 0);
-		q_a			: OUT STD_LOGIC_VECTOR (7 DOWNTO 0)
+		address_a	: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+		q_a			: OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
 	);
 	END COMPONENT;
 
@@ -53,15 +52,15 @@ BEGIN
 		address_aclr_a => "NONE",
 		clock_enable_input_a => "BYPASS",
 		clock_enable_output_a => "BYPASS",
-		init_file => "tcgrom.mif",
+		init_file => "image_data.mif",
 		intended_device_family => "Cyclone III",
 		lpm_hint => "ENABLE_RUNTIME_MOD=NO",
 		lpm_type => "altsyncram",
-		numwords_a => 247420,
+		numwords_a => 256,
 		operation_mode => "ROM",
 		outdata_aclr_a => "NONE",
 		outdata_reg_a => "UNREGISTERED",
-		widthad_a => 17,
+		widthad_a => 8,
 		width_a => 16,
 		width_byteena_a => 1
 	)
@@ -71,7 +70,7 @@ BEGIN
 		q_a => rom_data
 	);
 
-	rom_address <= "00" & font_row & "0000" + font_col;
-	rom_mux_output <= rom_data (CONV_INTEGER(NOT font_col(2 DOWNTO 0)));
+	rom_address <= font_row & font_col;
+	bird_data_output <= rom_data (CONV_INTEGER(NOT font_col(2 DOWNTO 0)));
 
 END SYN;
