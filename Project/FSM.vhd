a6c86994dceb684f0622d3d963ceb2eeb7a8b9e3
vdis663 <126431339+vdis663@users.noreply.github.com>
2024-05-27 21:41:11 +1200

    Update FSM.vhd

--- Project/FSM.vhd
+++ Project/FSM.vhd
@@ -11,52 +11,78 @@ ENTITY FSM IS
 END ENTITY;
 
 ARCHITECTURE BEHAVIOUR OF FSM IS
-	SIGNAL game_mode: STD_LOGIC_VECTOR(1 downto 0) := "00";
-BEGIN 
+	TYPE mode_type IS (Main_menu, Training, Normal, End_screen);
+	TYPE level_type IS (Easy, Medium, Hard);
+	SIGNAL current_state, next_state: mode_type;
+	SIGNAL current_level, next_level: level_type;
 	
-	FSM_game_mode: PROCESS(select_input, game_over)
+BEGIN 
+	CLOCK: PROCESS(clk)
+	begin
+		if(rising_edge(clk)) then
+			current_state <= next_state;
+			current_level <= next_level;
+		end if;
+	end process;
+	
+	NEXT_STATE_DECODE: PROCESS(select_input, game_over, score_in)
 		variable prev_select: STD_LOGIC;
 	begin
 		if(reset = '1') then
-			game_mode <= "00";
-			game_level_out <= "01";
-		elsif(rising_edge(clk)) then
-			if(prev_select = '1' and select_input = '0') then
-				if(game_mode = "00") then
-					if(option_input = '0') then
-						game_mode <= "10";
-						game_level_out <= "01";
-					elsif(option_input = '1') then
-						game_mode <= "01";
-						game_level_out <= "01";
+			next_state <= Main_menu;
+			next_level <= Easy;
+		else
+			if(prev_select = '1' and select_input = '0') then -- option selected with right click
+				if(current_state = Main_menu) then -- in Main Menu
+					if(option_input = '0') then -- switch to training mode
+						next_state <= Training;
+						next_level <= Easy;
+					elsif(option_input = '1') then -- switch to normal mode
+						next_state <= Normal;
+						next_level <= Easy;
 					end if;
-				elsif(game_mode = "11") then
-					if(option_input = '0') then
-						game_mode <= "10";
-						game_level_out <= "01";
-					elsif(option_input = '1') then
-						game_mode <= "00";
+				elsif(current_state = End_screen) then -- in Game Over
+					if(option_input = '0') then -- switch to main menu
+						next_state <= Normal;
+						next_level <= Easy;
+					elsif(option_input = '1') then -- switch to normal mode
+						next_state <= Main_menu;
 					end if;
 				end if;
-			elsif (game_over = '1') then
-				if(game_mode = "01") then 
-					game_mode <= "11";
-				elsif(game_mode = "10") then
-					game_mode <= "11";
+			elsif (game_over = '1') then -- no lives left
+				if(current_state = Normal or current_state = Training) then 
+					next_state <= End_screen;
 				end if;
 			end if;
 			
-			IF(game_mode = "10") THEN
-				IF(score_in > 30) THEN
-					game_level_out <= "11";
-				ELSIF(score_in <= 30 and score_in > 15) THEN
-					game_level_out <= "10";
+			IF(current_state = Normal) THEN -- in normal mode
+				IF(score_in > 30) THEN -- switch to hard
+					next_level <= Hard;
+				ELSIF(score_in <= 30 and score_in > 15) THEN -- switch to medium
+					next_level <= Easy;
 				END IF;
 			END IF;
 			prev_select := select_input;
 		end if;
-
-	end process FSM_game_mode;
+	end process;
 	
-	game_mode_out <= game_mode;
+	OUTPUT_DECODE : PROCESS(current_state, current_level)
+	begin
+		case(current_state) is
+			when Main_menu => game_mode_out <= "00";
+			when Training => game_mode_out <= "01";
+			when Normal => game_mode_out <= "10";
+			when End_screen => game_mode_out <= "11";
+			when others => game_mode_out <= "00";
+		end case;
+		
+		case(current_level) is
+			when Easy => game_level_out <= "01";
+			when Medium => game_level_out <= "10";
+			when Hard => game_level_out <= "11";
+			when others => game_level_out <= "01";
+		end case;
+	end process;
+	
+	
 end architecture;
